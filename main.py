4d97b4a811e40ba47cc8e1687f30f1f9b27b65e9
Gabriel Batistuta <batistutag190@gmail.com>
2023-11-22 09:15:05 -0300

    add test files

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import core
 
 # livro King Mombo do site 'gutenberg.org'
-DOMAIN_URL = 'https://www.gutenberg.org/cache/epub/62710/pg62710-images.html'
+DOMAIN_URL = 'https://openlibrary.org/works/OL3036163W/A_life_of_William_Shakespeare'
 
-source_html = core.get_source_html_from_url(DOMAIN_URL)
-print(source_html)
+book = 'https://archive.org/details/lifeofwilliamsha0000adam_x5z2/page/14/mode/2up?ref=ol&view=theater'
+
+source_html = core.get_source_html_from_url(book)
+print(source_html.prettify())
+
+with open('.html', 'w') as file:
+    file.write(source_html.prettify())
