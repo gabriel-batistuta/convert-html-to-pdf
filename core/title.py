d592f7cdd9311abe4dce108c0d5247336f6992c4
gabriel <batistutag190@gmail.com>
2023-11-29 21:23:50 -0300

    adicionados comentários ao arquivo title

--- core/title.py
+++ core/title.py
@@ -1,7 +1,14 @@
 def get_title(source_html):
     # extraindo o texto da tag titulo
-    title_text = source_html.find('h1').text.strip()
+    title_tag = source_html.find('h1')
+    print(title_tag)
+    # <h1>Alice’s Adventures in Wonderland</h1>
+
+    title_text = title_tag.text.strip()
     title = title_text.replace('\n','  ').replace('  ', ' ')
     print(f'título: {title} || {__name__}')
-    
+    # título: Alice’s Adventures in Wonderland || core.title
+     
     return title
+
+    
